b78b1cb2bb236388b75d5682ad9fdfe9723fa5c1
Pranesh Joshi <139208152+pranesh25joshi@users.noreply.github.com>
2024-06-17 11:48:28 +0530

    Time: 6 ms (27.80%) | Memory: 7.2 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -6,12 +6,9 @@ public:
     bool judgeSquareSum(int c) {
         long a = 0;
         double b;
-        if(isInteger(sqrt(c))){
-            return true;
-        }
         while(a*a <= c ){
-            b = c - a*a;
-            if(isInteger(sqrt(b))){
+            b = sqrt(c - a*a);
+            if(isInteger(b)){
                 return true;
             }
             a++;
