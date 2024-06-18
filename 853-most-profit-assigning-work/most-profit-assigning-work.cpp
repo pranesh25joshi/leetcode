b6758cf929c009451bb43eb39587b3913aec8bb4
Pranesh Joshi <139208152+pranesh25joshi@users.noreply.github.com>
2024-06-18 22:11:18 +0530

    Time: 2738 ms (5.02%) | Memory: 37.6 MB (99.61%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -2,34 +2,18 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
                             vector<int>& worker) {
-        // Create pairs of (difficulty, profit) and sort by difficulty
-        vector<pair<int, int>> jobs;
-        for (int i = 0; i < difficulty.size(); ++i) {
-            jobs.emplace_back(difficulty[i], profit[i]);
-        }
-        sort(jobs.begin(), jobs.end());
-
-        // Sort the workers by their abilities
-        sort(worker.begin(), worker.end());
-
-        int maxProfit = 0;
-        int maxProfitSoFar = 0;
-        int jobIndex = 0;
-
-        // Iterate over each worker
-        for (int ability : worker) {
-            // While the worker can do the current job, update the max profit so
-            // far
-            while (jobIndex < jobs.size() && jobs[jobIndex].first <= ability) {
-                maxProfitSoFar = max(maxProfitSoFar, jobs[jobIndex].second);
-                ++jobIndex;
+        int m = worker.size();
+        int n = profit.size();
+        int maxi = 0;
+        for (int i = 0; i < m; i++) {
+            int maxe = 0;
+            for (int j = 0; j < n; j++) {
+                if (difficulty[j] <= worker[i]) {
+                    maxe = max(maxe, profit[j]);
+                }
             }
-
-            // Add the best job's profit the worker can do to the total max
-            // profit
-            maxProfit += maxProfitSoFar;
+            maxi = maxi + maxe;
         }
-
-        return maxProfit;
+        return maxi;
     }
 };
