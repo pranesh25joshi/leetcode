36e462b6eaef34588c1ce1de8ce3eb31fac8df89
Pranesh Joshi <139208152+pranesh25joshi@users.noreply.github.com>
2024-06-22 22:38:44 +0530

    Time: 19 ms (86.73%) | Memory: 34.3 MB (48.80%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -2,39 +2,33 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int n = customers.size();
-        int unrealizedCustomers = 0;
+        int initialSatisfied = 0;
 
-        // Calculate initial number of unrealized customers in first 'minutes'
-        // window
-        for (int i = 0; i < minutes; i++) {
-            unrealizedCustomers += customers[i] * grumpy[i];
+        for (int i = 0; i < n; ++i) {
+            if (grumpy[i] == 0) {
+                initialSatisfied += customers[i];
+            }
         }
 
-        int maxUnrealizedCustomers = unrealizedCustomers;
-
-        // Slide the 'minutes' window across the rest of the customers array
-        for (int i = minutes; i < n; i++) {
-            // Add the current minute's unsatisfied customers if the owner is
-            // grumpy and remove the customers that are out of the current
-            // window
-            unrealizedCustomers += customers[i] * grumpy[i];
-            unrealizedCustomers -= customers[i - minutes] * grumpy[i - minutes];
-
-            // Update the maximum unrealized customers
-            maxUnrealizedCustomers =
-                max(maxUnrealizedCustomers, unrealizedCustomers);
+        int extraSatisfied = 0;
+        for (int i = 0; i < minutes; ++i) {
+            if (grumpy[i] == 1) {
+                extraSatisfied += customers[i];
+            }
         }
 
-        // Start with maximum possible satisfied customers due to secret
-        // technique
-        int totalCustomers = maxUnrealizedCustomers;
+        int maxExtraSatisfied = extraSatisfied;
 
-        // Add the satisfied customers during non-grumpy minutes
-        for (int i = 0; i < n; i++) {
-            totalCustomers += customers[i] * (1 - grumpy[i]);
+        for (int i = minutes; i < n; ++i) {
+            if (grumpy[i] == 1) {
+                extraSatisfied += customers[i];
+            }
+            if (grumpy[i - minutes] == 1) {
+                extraSatisfied -= customers[i - minutes];
+            }
+            maxExtraSatisfied = max(maxExtraSatisfied, extraSatisfied);
         }
 
-        // Return the maximum number of satisfied customers
-        return totalCustomers;
+        return initialSatisfied + maxExtraSatisfied;
     }
 };
