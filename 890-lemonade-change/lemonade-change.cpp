3084a7d73592e4f15d7fcf78fc1b5361fe034e96
Pranesh Joshi <139208152+pranesh25joshi@users.noreply.github.com>
2024-08-15 20:30:58 +0530

    Time: 86 ms (7.76%) | Memory: 86 MB (50.86%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -14,15 +14,14 @@ public:
                     return false;
             }
             else{
-                if( c10 > 0){
-                    c10--;
+                if (c5 > 0 && c10 > 0) {
                     c5--;
-                }
-                else{
-                    c5 = c5 - 3;
-                }
-                if( c5 < 0 )
+                    c10--;
+                } else if (c5 > 2) {
+                    c5 -= 3;
+                } else {
                     return false;
+                }
             }
         }
         return true;
