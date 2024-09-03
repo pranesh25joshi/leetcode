ef9bd369714b9b600cd568effb9c035388fc2e71
Pranesh Joshi <139208152+pranesh25joshi@users.noreply.github.com>
2024-09-03 20:33:06 +0530

    Time: 3 ms (51.28%) | Memory: 10.3 MB (11.28%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int transform( string convert, int k){
+        while( k > 0){
+            int sum = 0;
+            for( auto i : convert ){
+                sum += i - '0';
+            }
+            convert = to_string(sum);
+            k--;
+        }
+        return stoi(convert);
+    }
+    int getLucky(string s, int k) {
+        string convert = ""; 
+        for( auto i : s){
+            int num = i - 'a' + 1;
+            convert += std::to_string(num);
+        }
+        return transform( convert, k );
+
+    }
+};
